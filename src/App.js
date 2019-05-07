cc90309d5e21c1aac88bcb4bd512e48ff453c40c
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-05-07 11:20:02 -0500

    carptas con estrutura primeros pasos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola desde React</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <nav>
+          <ul>
+            <li>Desayuno</li>
+            <li>Hamburguesas</li>
+            <li>Bebidas</li>
+            <li>Complemntos</li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
