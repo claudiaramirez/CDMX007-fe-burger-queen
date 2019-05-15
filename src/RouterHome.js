a5079b28a99533c6089d450c819f7816001a1e82
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-05-15 14:55:17 -0500

    json con map y filter para en el correspondiente boton

--- src/RouterHome.js
+++ src/RouterHome.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './views/home';
 import Pagar from './views/pagar';
-//import login from './views/login';
 import Navbar from './components/navbar/navbar';
 import TotalProducts from './components/total/total';
 import Products from './components/products/products';
@@ -12,18 +11,14 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      {/* <header className="App-header"> */}
         <Navbar title="icon home" />
         <TotalProducts title="products" />
-        {/* <Route exact path="/" render={() => <Home title="Hola desde home" /> } /> */}
         <Route exact path="/" component={Home} />
         <Route path="/pagar" component={Pagar} />
-        {/* <Route path="/login" component={login} /> */}
         <Route path="/breakfast" component={Products} />
         <Route path="/hamburger" component={Products} />
         <Route path="/drinks" component={Products} />
         <Route path="/complements" component={Products} /> 
-       {/* </header>  */}
     </div>
     </BrowserRouter>
   );
--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,18 +1,11 @@
-// const Products = (props) =>(
-//     <div className='products'>
-//         <h2>Productos</h2>
-        
-//     </div>
-// );
-
-// export default Products;
 import React, {Component} from 'react';
 import './products.css';
+import RouterHome from '../../RouterHome';
+import {Route} from 'react-router-dom';
 class Products extends Component{
     constructor(props){
         super(props);
         this.state = {
-            // data:[],
             items: [],
             isLoaded: false,
         }
@@ -28,88 +21,94 @@ class Products extends Component{
                 items: findresponse, 
             })
         });
-
-        // fetch('https://burgerqueenmx.firebaseio.com/products.json')
-        // .then((Response) => Response.json())
-        // // if hamburger - muestrame sòlo ese pedazo del json
-        // .then((findresponse) =>
-        // {
-        //     console.log(findresponse[0].breakfast);
-        //     this.setState({
-        //         data:findresponse,
-        //     })
-        // })
-
-
-        // fetch ('https://burgerqueenmx.firebaseio.com/products.json')
-        //   .then(res => res.json())
-        //   .then(res => {
-        //     console.log(res);
-        //     if (res.success) {
-        //       //mensaje correcto
-             
-        //     }else{
-        //     //mensaje de error
-        //     }
-        //   })
-        //   .catch(function() {
-        //     alert("JSON NO ENCONTRADO");
-        //   });
     }
     render()
-    // {
-    //     return(
-    //         <div>
-    //             {
-    //                 this.state.data.map((item,key) =>  
-    //                 <li key ={key.id}>
-    //                     <p>{item.price}</p>
-    //                     <p>{item.img}</p>
-    //                 </li>    
-    //                )   
-    //             }
-    //         </div>    
-    //     )   
-    // }
     {
         var { isLoaded, items} = this.state;
         if(!isLoaded){
             return <div>Cargando...</div>
-        } else{
+        } 
+        else if(window.location.href.includes('breakfast')){
             return(
                 <div className="Products">
-                    
-                        {items.map(item =>(
-                            // <div key={item.id}>
-                            //     product : {item.product} 
-                            //     <img src={item.img}/>
-                            //     price : {item.price}  
-                            // </div>
-                            <div class="col s3 m3 l3">
-                                <div class="card horizontal">
-                                <div class="card-image">
-                                    <img src={item.img}/>
+                    {items.filter(item => item.type === "breakfast").map(item => 
+                        <div class="col s3 m3 l3">
+                            <div class="card horizontal">
+                            <div class="card-image">
+                                <img src={item.img}/>
+                            </div>
+                            <div class="card-stacked">
+                                <div class="card-content">
+                                    <p>{item.product}</p>
+                                    <p>{item.price}</p>
                                 </div>
-                                <div class="card-stacked">
-                                    <div class="card-content">
-                                        <p>{item.product}</p>
-                                        <p>{item.price}</p>
-                                    </div>
-                                    <div class="card-action">
-                                        <a href="#">Agregar</a>
-                                    </div>
-                                    </div>
                                 </div>
                             </div>
-                        ))}
-                   
-                     
+                        </div>
+                    )}     
                 </div>    
             );
-        }    
+        }else if(window.location.href.includes('hamburger')){
+            return(
+                <div className="Products">
+                    {items.filter(item => item.type === "hamburguer").map(item => 
+                        <div class="col s3 m3 l3">
+                            <div class="card horizontal">
+                            <div class="card-image">
+                                <img src={item.img}/>
+                            </div>
+                            <div class="card-stacked">
+                                <div class="card-content">
+                                    <p>{item.product}</p>
+                                    <p>{item.price}</p>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}       
+                </div>    
+            );
+        }else if(window.location.href.includes('drinks')){
+            return(
+                <div className="Products">
+                    {items.filter(item => item.type === "drinks").map(item => 
+                        <div class="col s3 m3 l3">
+                            <div class="card horizontal">
+                            <div class="card-image">
+                                <img src={item.img}/>
+                            </div>
+                            <div class="card-stacked">
+                                <div class="card-content">
+                                    <p>{item.product}</p>
+                                    <p>{item.price}</p>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}       
+                </div>    
+            );
+        }else if(window.location.href.includes('complements')){
+            return(
+                <div className="Products">
+                    {items.filter(item => item.type === "complements").map(item => 
+                        <div class="col s3 m3 l3">
+                            <div class="card horizontal">
+                            <div class="card-image">
+                                <img src={item.img}/>
+                            </div>
+                            <div class="card-stacked">
+                                <div class="card-content">
+                                    <p>{item.product}</p>
+                                    <p>{item.price}</p>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    )}       
+                </div>    
+            );
+        }   
     }
 }
 export default Products;
-
-
-
